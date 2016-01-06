ff6fb6b6337668c99d156d020591023fdaf82a09
Regencyop <GitHub@regencyop.com>
2016-01-06 14:52:25 -0500

    Update routing.js

--- app/js/routing.js
+++ app/js/routing.js
@@ -10,13 +10,33 @@ four51.app.config(['$routeProvider', '$locationProvider', function($routeProvide
 	}
 
 	$routeProvider.
-		when('/listOrders', { templateUrl: 'partials/listOrders.html', controller: 'ListOrdersCtrl' }).
-		when('/orderdetails/:orderid', {templateUrl: 'partials/orderDetails.html', controller: 'OrderDetailsCtrl'}).
-        when('/catalog', { templateUrl: 'partials/categoryView.html', controller: 'CategoryCtrl' }).
-        when('/catalog/:categoryInteropID', { templateUrl: 'partials/categoryView.html', controller: 'CategoryCtrl' }).
-        when('/product/:productInteropID', {templateUrl: concatProductView, controller: 'ProductCtrl'}).
-        when('/product/:productInteropID/:variantInteropID', {templateUrl: concatProductView, controller: 'ProductCtrl'}).
-		when('/product/:productInteropID/:variantInteropID/edit', {templateUrl: concatSpecFormView, controller: 'SpecFormCtrl'}).
+		when('/listOrders', 
+		    { templateUrl: 'partials/listOrders.html', controller: 'ListOrdersCtrl' }).
+		    
+		when('/orderdetails/:orderid', 
+		    {templateUrl: 'partials/orderDetails.html', controller: 'OrderDetailsCtrl'}).
+		    
+        when('/catalog', 
+            { templateUrl: 'partials/categoryView.html', controller: 'CategoryCtrl' }).
+
+       	//40849-konaWearAll 
+        //come back to this 
+        //when('/catalog/40849-konaWearAll', 
+        //    {templateUrl: concatProductView, controller: 'CategoryCtrlApparel'}).  
+            
+        when('/catalog/:categoryInteropID', 
+            { templateUrl: 'partials/categoryView.html', controller: 'CategoryCtrl' }).
+            
+        when('/product/:productInteropID', 
+            {templateUrl: concatProductView, controller: 'ProductCtrl'}).
+        
+        when('/product/:productInteropID/:variantInteropID', 
+            {templateUrl: concatProductView, controller: 'ProductCtrl'}).          
+            
+		when('/product/:productInteropID/:variantInteropID/edit', 
+		    {templateUrl: concatSpecFormView, controller: 'SpecFormCtrl'}).
+		    
+		//untouched
 		when('/order', { templateUrl: 'partials/orderSearchView.html', controller: 'OrderSearchCtrl' }).
 		when('/order/:id', { templateUrl: 'partials/Reporting/orderHistoryView.html', controller: 'OrderViewCtrl' }).
 		when('/favoriteorders', { templateUrl: 'partials/favoriteOrderListView.html', controller: 'FavoriteOrderCtrl' }).
